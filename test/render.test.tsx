ee27758e34ce859a7b2bd329a2c4984e504b5c7a
Igor Matias <matyas.igor@gmail.com>
2020-09-09 20:42:11 +0200

    fix: update tests

--- test/render.test.tsx
+++ test/render.test.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Default as VirtualList } from '../stories/VirtualList.stories'
+import VirtualList, { ItemStyle } from '../src'
+
+const STICKY_INDICES = [0, 5, 8, 15, 30, 50, 100, 200]
+const ITEM_STYLE: React.CSSProperties = {
+  padding: 8,
+  boxSizing: 'border-box',
+  fontFamily: 'sans-serif',
+}
+const renderItem = ({ style, index }: { style: ItemStyle; index: number }) => {
+  const isSticky = STICKY_INDICES.includes(index)
+  const itemStyle = isSticky ? { ...style, ...ITEM_STYLE, backgroundColor: '#eee' } : { ...style, ...ITEM_STYLE }
+  return (
+    <div style={itemStyle} key={index}>
+      Row #{index} {isSticky ? '(Sticky)' : ''}
+    </div>
+  )
+}
 
 describe('VirtualList', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<VirtualList />, div)
+    ReactDOM.render(
+      <VirtualList
+        width="auto"
+        height={400}
+        itemCount={1000}
+        renderItem={renderItem}
+        itemSize={52}
+        stickyIndices={STICKY_INDICES}
+      />,
+      div
+    )
     ReactDOM.unmountComponentAtNode(div)
   })
 })
