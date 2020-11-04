901473f4b192eed4d4a4bae723f46c1503e6f656
Igor Matias <matyas.igor@gmail.com>
2020-11-04 23:49:42 +0300

    chore: update virtual list examples

--- stories/VirtualList.stories.tsx
+++ stories/VirtualList.stories.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react'
-import VirtualList, { ItemStyle, Props, ScrollAlignment } from '../src'
+import VirtualList, { ItemStyle, ScrollAlignment, ScrollDirection } from '../src'
 
 export default {
   title: 'VirtualList',
 }
 
 const STICKY_INDICES = [0, 5, 8, 15, 30, 50, 100, 200]
+const ITEM_HEIGHT = 48
 const ITEM_STYLE: React.CSSProperties = {
-  padding: 8,
+  padding: '12px 20px',
   boxSizing: 'border-box',
-  fontFamily: 'sans-serif',
+  fontFamily: 'system-ui, "Helvetica Neue", Helvetica, sans-serif',
+  fontSize: '16px',
+  lineHeight: '24px',
 }
 
 const renderItem = ({ style, index }: { style: ItemStyle; index: number }) => {
   const isSticky = STICKY_INDICES.includes(index)
-  const itemStyle = isSticky ? { ...style, ...ITEM_STYLE, backgroundColor: '#eee' } : { ...style, ...ITEM_STYLE }
+  const itemStyle = isSticky ? { ...style, ...ITEM_STYLE, backgroundColor: '#f7f7f7' } : { ...style, ...ITEM_STYLE }
   return (
     <div style={itemStyle} key={index}>
       Row #{index} {isSticky ? '(Sticky)' : ''}
@@ -22,34 +25,82 @@ const renderItem = ({ style, index }: { style: ItemStyle; index: number }) => {
   )
 }
 
-export const Default = (props?: Partial<Props>) => (
-  <VirtualList
-    width="auto"
-    height={400}
-    itemCount={1000}
-    renderItem={renderItem}
-    itemSize={52}
-    stickyIndices={STICKY_INDICES}
-    {...props}
-  />
+export const Default = () => (
+  <div style={{ border: '3px solid #edeff0' }}>
+    <VirtualList
+      width="auto"
+      height={400}
+      itemCount={1000}
+      renderItem={renderItem}
+      itemSize={ITEM_HEIGHT}
+      stickyIndices={STICKY_INDICES}
+      scrollDirection={ScrollDirection.VERTICAL}
+      overscanCount={5}
+    />
+  </div>
 )
 
-export const Controlled = (props?: Partial<Props>) => {
-  const [index, setIndex] = React.useState(null)
+export const Controlled = () => {
+  const [index, setIndex] = React.useState<number | null>(null)
   const setNewIndex = React.useCallback(index => {
     setIndex(null)
     setTimeout(() => setIndex(index))
   }, [])
   return (
-    <div>
-      <Default scrollToIndex={index} {...props} />
-      <div style={{ paddingTop: 16 }}>
+    <>
+      <div style={{ marginBottom: 16 }}>
         <button onClick={() => setNewIndex(5)}>Scroll to 5</button>
+        &nbsp;
         <button onClick={() => setNewIndex(15)}>Scroll to 15</button>
+        &nbsp;
         <button onClick={() => setNewIndex(30)}>Scroll to 30</button>
       </div>
-    </div>
+      <div style={{ border: '3px solid #edeff0' }}>
+        <VirtualList
+          width="auto"
+          height={400}
+          itemCount={1000}
+          renderItem={renderItem}
+          itemSize={ITEM_HEIGHT}
+          stickyIndices={STICKY_INDICES}
+          scrollToIndex={index !== null ? index : undefined}
+          scrollDirection={ScrollDirection.VERTICAL}
+          overscanCount={5}
+        />
+      </div>
+    </>
   )
 }
 
-export const Smart = (props?: Partial<Props>) => <Controlled scrollToAlignment={ScrollAlignment.SMART} />
+export const Smart = () => {
+  const [index, setIndex] = React.useState<number | null>(null)
+  const setNewIndex = React.useCallback(index => {
+    setIndex(null)
+    setTimeout(() => setIndex(index))
+  }, [])
+  return (
+    <>
+      <div style={{ marginBottom: 16 }}>
+        <button onClick={() => setNewIndex(5)}>Scroll to 5</button>
+        &nbsp;
+        <button onClick={() => setNewIndex(15)}>Scroll to 15</button>
+        &nbsp;
+        <button onClick={() => setNewIndex(30)}>Scroll to 30</button>
+      </div>
+      <div style={{ border: '3px solid #edeff0' }}>
+        <VirtualList
+          width="auto"
+          height={400}
+          itemCount={1000}
+          renderItem={renderItem}
+          itemSize={ITEM_HEIGHT}
+          stickyIndices={STICKY_INDICES}
+          scrollToIndex={index !== null ? index : undefined}
+          scrollDirection={ScrollDirection.VERTICAL}
+          scrollToAlignment={ScrollAlignment.SMART}
+          overscanCount={5}
+        />
+      </div>
+    </>
+  )
+}
